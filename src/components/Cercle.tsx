46addd623a04f010ef4d5a794e941f00f6af4036
zryqv <mehdizibout@gmail.com>
2022-09-17 01:00:22 +0100

    fix gradient mistake in the cercle

--- src/components/Cercle.tsx
+++ src/components/Cercle.tsx
@@ -66,7 +66,7 @@ function Cercle({
       style={{ boxShadow: '50px 50px 100px 0px rgba(18, 21, 48, 1)' }}
     >
       <div
-        className='box rounded-full tablet:w-[25.625rem] tablet:h-[25.625rem] w-[18.75rem] h-[18.75rem] bg-gradient-to-br from-[#2E325A] to-[#0E112A] flex justify-center items-center'
+        className='box rounded-full tablet:w-[25.625rem] tablet:h-[25.625rem] w-[18.75rem] h-[18.75rem] bg-gradient-to-br  from-[#0E112A] to-[#2E325A] flex justify-center items-center'
         style={{ boxShadow: '-50px -50px 100px 0px rgba(39, 44, 90, 1)' }}
       >
         <div className='bg-black rounded-full h-[16.73rem] w-[16.73rem] tablet:w-[22.875rem] tablet:h-[22.875rem] relative'>
