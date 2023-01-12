e499cbd7b2d23f433ded6b51711a59a6b802b800
mehdi-zibout <mehdi-zibout@users.noreply.github.com>
2023-01-12 10:23:06 +0000

    deploy: eeca25d8eb789a544b807478920d511eb7e2f9a6

--- worker.js
+++
@@ -1,8 +0,0 @@
-onmessage = (e) => {
-  console.log("Message received from main script");
-  let remaining = e.data;
-  setTimeout(() => {
-    remaining -= 1 / 60;
-    postMessage(remaining);
-  }, 1000);
-};
